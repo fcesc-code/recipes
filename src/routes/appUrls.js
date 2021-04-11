8fc2c528fbe3f997825212dbacdb22e4bc376e3c
fcesc-code <bpfran@gmail.com>
2021-04-11 16:00:56 +0200

    feature / upgraded design

---
+++ src/routes/appUrls.js
@@ -0,0 +1,8 @@
+const ROUTES = [
+  { title: 'home', path: '/', url: '' },
+  { title: 'categories', path: '/categories/', url: '' },
+  { title: 'about', path: '/about/', url: '' },
+  { title: 'credits', path: '/credits/', url: '' }
+];
+
+export default ROUTES;
--- src/views/header.js
+++ src/views/header.js
@@ -1,25 +1,33 @@
 import renderComponent from '../core/renderer';
 import MOCKDATA from '../mockdata/recipiesList';
+import APPURLS from '../routes/appUrls';
 
 const rawList = MOCKDATA.map( recipe => recipe.category );
 const eliminateDuplicates = [ ...new Set(rawList) ];
-const neatList = eliminateDuplicates.map( item => { return { 'category': item } })
-const itemTemplate = `<li>{{category}}</li>`;
+const categoriesList = eliminateDuplicates.map( item => { return { 'category': item } })
+const categoryTemplate = `<li>{{category}}</li>`;
+
+const urlList = APPURLS;
+const navTemplate = `<li><a href="{{url}}">{{title}}</a></li>`;
 
 function headerComponent(){
   renderComponent(`
-    <h1>RECEPTES</h1>
-    <nav><ul class="navMenu">{{%%categories%%}}</ul></nav>
-    <p>{{someContent}} {{andMore}}</p>
+    <h1 class="mainTitle">RECEPTES</h1>
+    <p>{{emblem}}</p>
+    <nav><ul class="navMenu">{{%%navMenu%%}}</ul></nav>
+    <ul class="categoriesMenu">{{%%categories%%}}</ul>
   `)({
     parent: 'header',
     styles: null,
     data: {
-      someContent: 'This is fun!',
-      andMore: 'A lot of fun.',
+      emblem: 'Quick, romantic, healthy or delicious cooking? Find recipies for all your needs.',
       categories: {
-        list: neatList,
-        itemTemplate
+        list: categoriesList,
+        itemTemplate: categoryTemplate
+      },
+      navMenu: {
+        list: urlList,
+        itemTemplate: navTemplate
       }
     }
   });
